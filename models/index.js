9b4be9402199182e2521948b795df7547f05c591
Ashley Kohl <akohl22@gmail.com>
2021-05-10 22:27:06 -0600

    category api route

---
+++ models/index.js
@@ -0,0 +1,57 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+
+Product.belongsTo(Category, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: Tag,
+    foreignKey: 'category_id',
+  },
+  // Define an alias for when data is retrieved
+  as: 'product_category'
+});
+
+// Categories have many Products
+Category.belongsToMany(Product, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: Tag,
+    foreignKey: 'category_id',
+  },
+  // Define an alias for when data is retrieved
+  as: 'category_product'
+});
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    foreignKey: 'product_id'
+  },
+  // Define an alias for when data is retrieved
+  as: 'products_tag'
+});
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    foreignKey: 'tag_id'
+  },
+  // Define an alias for when data is retrieved
+  as: 'tag_product'
+});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
